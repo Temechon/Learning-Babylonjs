563f897a75bb5fdda3a03704b1a13bb9c895efda
Zacaria <zc@42.am>
2015-08-26 11:03:14 +0200

    added typed js to first sentence

--- js/index.js
+++ js/index.js
@@ -15,4 +15,12 @@
         scroll($("#learn_more"));
     });
 
+    $(".baseline-text").typed({
+        strings: ["Learn the basics^200 of the 3D framework Babylon.js^200 by creating a whole game. ^1000<br>This 160 pages e-book^300 will teach you^200 everything you need to know about Babylon.js^200 to get the most out of it!"],
+        showCursor: true,
+        contentType: 'html',
+        startDelay: 1000,
+        typeSpeed: 30
+    });
+
 })();
